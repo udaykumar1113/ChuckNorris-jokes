0a64c63e310e847ba7b7fa10b8ffdc4d157087e1
uday <udaykumar1113@gmail.com>
2017-11-09 21:51:06 -0500

    created jokes app

---
+++ src/main/java/com/jokes/ChucknorrisJokesApplication.java
@@ -0,0 +1,12 @@
+package com.jokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChucknorrisJokesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChucknorrisJokesApplication.class, args);
+	}
+}
